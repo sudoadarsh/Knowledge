32a5185506730f05eb6fccab3732658e65dc7748
Adarsh <adarshs@meditab.com>
2024-12-14 18:13:36 +0530

    initial commit

---
+++ KnowledgeTests/KnowledgeTests.swift
@@ -0,0 +1,17 @@
+//
+//  KnowledgeTests.swift
+//  KnowledgeTests
+//
+//  Created by Adarsh Sudarsanan on 14/12/24.
+//
+
+import Testing
+@testable import Knowledge
+
+struct KnowledgeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
